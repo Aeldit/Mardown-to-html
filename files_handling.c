c516b54bfae76df2a5a3229a7890536bf9ec3383
Aeldit <raphael.roger.92@gmail.com>
2024-01-22 22:42:20 +0100

    Started working on reading a file

--- files_handling.c
+++ files_handling.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "files_handling.h"
 
@@ -7,6 +8,19 @@ fc_control_t* read_file(char* path)
 	FILE* f = fopen(path, "r");
 	if (f == NULL)
 	{
+		return NULL;
+	}
+
+	fc_control_t* fcc = calloc(1, sizeof(fc_control_t));
+	if (fcc == NULL)
+	{
+		return NULL;
+	}
+
+	file_content_t* fc = calloc(1, sizeof(file_content_t));
+	if (fc == NULL)
+	{
+		free(fcc);
 		return NULL;
 	}
 
@@ -23,6 +37,15 @@ fc_control_t* read_file(char* path)
 		if (fseek(f, lines++, SEEK_SET) != 0)
 		{
 			break;
+		}
+
+		if (buff_idx < FILE_BUFF_SIZE)
+		{
+			fc->buffer[buff_idx++] = c;
+		}
+		else
+		{
+			buff_idx = 0;
 		}
 	}
 	return NULL;
--- files_handling.h
+++ files_handling.h
@@ -1,9 +1,11 @@
 #ifndef FILES_HANDLING_H
 #define FILES_HANDLING_H
 
+#define FILE_BUFF_SIZE 512
+
 typedef struct
 {
-	char buffer[512];
+	char buffer[FILE_BUFF_SIZE];
 	struct file_content_t* next;
 } file_content_t;
 
@@ -12,5 +14,7 @@ typedef struct
 	size_t nb_buffers;
 	file_content_t* head;
 } fc_control_t;
+
+fc_control_t* read_file(char* path);
 
 #endif
